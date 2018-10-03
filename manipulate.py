78eff39c27dffac8d18f5ebf01a5c777f4f0ddcb
Ethiraj <43022487+ethirajkotiah@users.noreply.github.com>
2018-10-03 15:23:18 -0400

    Update manipulate.py

--- manipulate.py
+++ manipulate.py
@@ -25,34 +25,4 @@ def df_derived_by_shift(df,lag=0,NON_DER=[]):
             i+=1
         df = pd.concat([df, dfn], axis=1, join_axes=[df.index])
     return df
-
-def autocorrplots(df_S,col_name,selectedLagPoints,maxLagDays):
-    #%% show autocorr and lag plots
-    temperatureDF=df_S
-    col_name=cityToShow
-    originalSignal = temperatureDF[cityToShow]
-    # set grid spec of the subplots
-    plt.figure(figsize=(12,6))
-    gs = gridspec.GridSpec(2, len(selectedLagPoints))
-    axTopRow = plt.subplot(gs[0, :])
-    axBottomRow = []
-    for i in range(len(selectedLagPoints)):
-        axBottomRow.append(plt.subplot(gs[1, i]))
-
-    # plot autocorr
-    allTimeLags = np.arange(1,maxLagDays*24)
-    autoCorr = [originalSignal.autocorr(lag=dt) for dt in allTimeLags]
-    axTopRow.plot(allTimeLags,autoCorr); 
-    axTopRow.set_title('autocorr Plot', fontsize=18);
-    axTopRow.set_xlabel('time lag'); axTopRow.set_ylabel('correlation coefficient')
-    selectedAutoCorr = [originalSignal.autocorr(lag=dt) for dt in selectedLagPoints]
-    axTopRow.scatter(x=selectedLagPoints, y=selectedAutoCorr, s=50, c='r')
-
-    # plot scatter plot of selected points
-    for i in range(len(selectedLagPoints)):
-        lag_plot(originalSignal, lag=selectedLagPoints[i], s=0.5, alpha=0.7, ax=axBottomRow[i])    
-        if i >= 1:
-            axBottomRow[i].set_yticks([],[])
-    plt.tight_layout()
-    plt.show()
     
