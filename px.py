93d6057cdad30b6bac653da63ad54f5daed502e1
Ethiraj <43022487+ethirajkotiah@users.noreply.github.com>
2018-10-17 12:09:11 -0400

    Update px.py

--- px.py
+++ px.py
@@ -1,15 +1,6 @@
-quandl.ApiConfig.api_key = "###"
-NG = quandl.get("CHRIS/CME_NG1", collapse="daily")
-
-no=range(1,10+1)
-pit='CHRIS/CME_'
-sym='NG'
-colnum='.6'
-x=[]
-for i in no:
-    z=pit.__add__(sym).__add__(str(i)).__add__(colnum)
-    x.append(z)
-##############################
+def quandlkey(a):
+    return quandl.ApiConfig.api_key = a
+#############################################################################################
 ##flat
 def flatfutures(a,b,c):
 #     no=range(1,10+1)
@@ -26,7 +17,7 @@ def flatfutures(a,b,c):
         x.append(z)
     dld = quandl.get(x)
     return dld
-##############################
+#############################################################################################
 ##timespread
 def timespread(a,b,c):
 #     no=range(1,10+1)
@@ -45,7 +36,6 @@ def timespread(a,b,c):
     df=dld
     df=df.diff(axis=1)
     return df
-##############################
 #############################################################################################
 def alltechindicators(NG):
     df = add_all_ta_features(NG, "Open", "High", "Low", "Last", "Volume", fillna=True)
