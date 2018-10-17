f7bf3d8bddacd2d079ea52d8e3fefca6c6c54420
Ethiraj <43022487+ethirajkotiah@users.noreply.github.com>
2018-10-17 12:07:23 -0400

    Update px.py

--- px.py
+++ px.py
@@ -11,9 +11,48 @@ for i in no:
     x.append(z)
 ##############################
 ##flat
-dld = quandl.get(x)
+def flatfutures(a,b,c):
+#     no=range(1,10+1)
+#     pit='CHRIS/CME_'
+#     sym='NG'
+#     colnum='.6'
+    no=a
+    pit=b
+    sym=c
+    colnum='.6'
+    x=[]
+    for i in no:
+        z=pit.__add__(sym).__add__(str(i)).__add__(colnum)
+        x.append(z)
+    dld = quandl.get(x)
+    return dld
 ##############################
 ##timespread
-df=dld
-df=df.diff(axis=1)
+def timespread(a,b,c):
+#     no=range(1,10+1)
+#     pit='CHRIS/CME_'
+#     sym='NG'
+#     colnum='.6'
+    no=a
+    pit=b
+    sym=c
+    colnum='.6'
+    x=[]
+    for i in no:
+        z=pit.__add__(sym).__add__(str(i)).__add__(colnum)
+        x.append(z)
+    dld = quandl.get(x)
+    df=dld
+    df=df.diff(axis=1)
+    return df
 ##############################
+#############################################################################################
+def alltechindicators(NG):
+    df = add_all_ta_features(NG, "Open", "High", "Low", "Last", "Volume", fillna=True)
+    return df
+#############################################################################################
+def smoothsvgfilter(df,a,b):
+    svgfilter = lambda x: savgol_filter(x,a,b)
+    df_smooth=df.apply(svgfilter)
+    return df_smooth
+#############################################################################################
